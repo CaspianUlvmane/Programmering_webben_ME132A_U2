b9760d3716c4034e8874dca4c77673dcb9a32416
Caspian Ulvmåne <cabbeulv@gmail.com>
2022-03-24 09:59:41 +0100

    RecommitU2

---
+++ database.js
@@ -0,0 +1,84 @@
+"use strict"
+
+let database = [
+  {
+    id: 1,
+    name: 'Alfred Hitchcock',
+    age: 80,
+    livingStatus: 'Dead',
+    numberOfAwards: 31,
+    directorialCredits: 69
+  },
+  {
+    id: 2,
+    name: 'Stanley Kubrick',
+    age: 70,
+    livingStatus: 'Dead',
+    numberOfAwards: 46,
+    directorialCredits: 16
+  },
+  {
+    id: 3,
+    name: 'Christopher Nolan',
+    age: 51,
+    livingStatus: 'Alive',
+    numberOfAwards: 141,
+    directorialCredits: 17
+  },
+  {
+    id: 4,
+    name: 'Martin Scorsese',
+    age: 79,
+    livingStatus: 'Alive',
+    numberOfAwards: 167,
+    directorialCredits: 68
+  },
+  {
+    id: 5,
+    name: 'Steven Spielberg',
+    age: 75,
+    livingStatus: 'Alive',
+    numberOfAwards: 196,
+    directorialCredits: 57
+  },
+  {
+    id: 6,
+    name: 'Quentin Tarantino',
+    age: 58,
+    livingStatus: 'Alive',
+    numberOfAwards: 172,
+    directorialCredits: 21
+  },
+  {
+    id: 7,
+    name: 'Wes Anderson',
+    age: 52,
+    livingStatus: 'Alive',
+    numberOfAwards: 77,
+    directorialCredits: 22
+  },
+  {
+    id: 8,
+    name: 'Cecil B. DeMille',
+    age: 77,
+    livingStatus: 'Dead',
+    numberOfAwards: 28,
+    directorialCredits: 81
+  },
+  {
+    id: 9,
+    name: 'Tim Burton',
+    age: 63,
+    livingStatus: 'Alive',
+    numberOfAwards: 27,
+    directorialCredits: 40
+  },
+  {
+    id: 10,
+    name: 'Ingmar Bergman',
+    age: 89,
+    livingStatus: 'Dead',
+    numberOfAwards: 85,
+    directorialCredits: 70
+  }
+]
